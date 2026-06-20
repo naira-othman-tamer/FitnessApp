3a2521f83766c70a8a039c57d7b848d0ab97c369
naira_tamer <naira.othman.Tamer@gmail.com>
2026-06-20 23:29:23 +0300

    Create WorkoutService and cofig it's i,age at yml file - PrepareDomain at FCE service

--- FCE/Domain/CalculatedMetrics.cs
+++ FCE/Domain/CalculatedMetrics.cs
@@ -1,6 +1,25 @@
-﻿namespace FCE.Domain
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FCE.Domain
 {
-    public class CalculatedMetrics
+    public enum Status
     {
+        Weak,
+        Normal,
+        Hard,
+    }   
+    public class CalculatedMetrics //stage 2 [Stores derived metabolic outputs]
+    {
+        public Guid userId { get; set; }
+        public double BMR { get; set; }
+        public double TDEE { get; set; }
+        public double calorieTarget { get; set; }
+        public Status status { get; set; }
+        public DateTime CalculatedAt { get; set; }
+    }
+
+    public class CalculatedMetricsConfig //: IEntityTypeConfiguration<CalculatedMetrics>
+    {
+        
     }
 }
--- FCE/Domain/UserFitnessStats.cs
+++ FCE/Domain/UserFitnessStats.cs
@@ -1,6 +1,15 @@
 ﻿namespace FCE.Domain
 {
-    public class UserFitnessStats
+    public enum Gender { Male, Female }
+    public class UserFitnessStats //stage1 [Stores raw physical input variables]
     {
+        public Guid userId { get; set; }
+        public double weight { get; set; }
+        public double Height { get; set; }
+        public short age { get; set; }
+        public Gender gender { get; set; }
+        public string goal { get; set; }
+        public string activityResult { get; set; }
+        public DateTime RecordedAt { get; set; }
     }
 }
